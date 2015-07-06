5632dcdb8b1c8022e27f28b36f610d002a0e2c56
JJR <jinjiren@foxmail.com>
2015-07-07 00:16:50 +0900

    Finish rbf kernel

--- rbf_svm.m
+++ rbf_svm.m
@@ -1,37 +1,43 @@
 % Soft-margin SVM
 
 % C Control the number of misclassifed data point
-C=100;  % The larger C is, the smaller the margin is.
-sig=1;  % variance of RBF kernel
-n=size(x',1);
+C=10;
+sig=0.5;
+% # of training data
+n=size(x,2);
+
+% Compute K in matrix form
 K=x'*x/sig^2;
 d=diag(K);
 K=K-ones(n,1)*d'/2;
 K=K-d*ones(1,n)/2;
 K=exp(K);
-Q=l'*l.*K;
+% Compute H
+H=l'*l.*K;
 
 options=optimset('Algorithm','interior-point-convex');
-alpha=quadprog(Q,-ones(1,size(x,2)),[],[],l,0,...
+alpha=quadprog(H,-ones(1,size(x,2)),[],[],l,0,...
     zeros(1,size(x,2)),C*ones(1,size(x,2)),[],options)';
 
 sv=alpha>1e-5;
 isv=find(sv);
 
-sum_j = 0;
-sum_i = 0;
 % compute b
+ff = zeros(length(isv),1);
 for index = 1:numel(isv)
     i = isv(index);
+    z = x(:,i);
     for jindex = 1:numel(isv)
-        j = isv(jindex);
-        Kj = exp(-(x(:,i) - x(:,j))' * (x(:,i) - x(:,j)) / (2*sig^2));
-        tmp1 = alpha(j)*l(j)*Kj - l(i);
-        sum_j = sum_j + tmp1;
+        elm = isv(jindex);
+        Kz = exp(-(x(:,elm) - z)' * (x(:,elm) - z) / (2*sig^2));
+        tmp = alpha(elm)*l(elm)*Kz;
+        ff(index,1) = ff(index,1) + tmp;
     end
-    sum_i = sum_i + sum_j;
 end
-b = sum_i / length(isv)
+b = sum(l(sv) - ff')/length(sv);
+
+
+
 
 
 z = randn(2,1);
@@ -53,24 +59,9 @@ for index = 1:numel(isv)
     tmp = alpha(elm)*l(elm)*Kz;
     f(i,1) = f(i,1)+tmp;
 end
-f(i,1) = f(i,1) + 0.0256;
+f(i,1) = f(i,1) + b;
 end
 
-
-ff = zeros(length(isv),1);
-% compute b
-for index = 1:numel(isv)
-    i = isv(index);
-    z = x(:,i);
-    for jindex = 1:numel(isv)
-        elm = isv(jindex);
-        Kz = exp(-(x(:,elm) - z)' * (x(:,elm) - z) / (2*sig^2));
-        tmp = alpha(elm)*l(elm)*Kz;
-        ff(index,1) = ff(index,1) + tmp;
-    end
-end
-ff
-sum(ff'-l(sv))/length(sv)
 f = reshape(f, 100, 100);
 
 %result = sign(f);
@@ -91,6 +82,7 @@ plot(x(1,find(l>0 & ~sv)),x(2,find(l>0 & ~sv)),'bo');
 plot(x(1,find(l<0 & sv)),x(2,find(l<0 & sv)),'rx');
 plot(x(1,find(l<0 & ~sv)),x(2,find(l<0 & ~sv)),'bx');
 
-contour(X1, X2, f, [-1 0 1])
+C = contour(X1, X2, f, [-1 0 1]);
+clabel(C)
 xlim([-1 1]);
 ylim([-1 1]);
